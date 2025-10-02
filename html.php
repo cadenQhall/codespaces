9b14604a2542fbbf48c87c09e98cea2962be0902
cadenQhall <cadturnburg@gmail.com>
2025-10-02 19:22:42 +0000

    Added something

---
+++ html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>HTML</title>
+    </head>
+    <body>
+        <h1 id="main-heading">Hello!</h1>
+        <p>My name is Caden.</p>
+        <div class="main">
+            <p style="color:blue">Hello, this is a paragraph.</p>
+        </div>
+    </body>
+</html>
