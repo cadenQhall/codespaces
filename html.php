5cbedc912602c27cccf997ad2ea2e05035f1d336
cadenQhall <cadturnburg@gmail.com>
2025-10-07 20:28:57 +0000

    Added Something

--- html.php
+++ html.php
@@ -9,7 +9,46 @@
         <h1 id="main-heading">Hello!</h1>
         <p>My name is Caden.</p>
         <div class="main">
-            <p style="color:blue">Hello, this is a paragraph.</p>
+            <p style="color:red">Hello, this is a paragraph.</p>
+            <a href="https://youtube.com" target="_blank">YouTube</a>
+            <a href="#main-heading">This is a heading.</a>
+            <form method="POST">
+                <p>
+                    <label for="email">Email</label>
+                    <input name="email" type="email" placeholder="Your email address..." />
+                </p>
+                <p>
+                    <label for="eye color">Eye color</label>
+                    <select name="eye color">
+                        <option value="blue">blue</option>
+                        <option value="green">green</option>
+                        <option value="brown">brown</option>
+                        <option value="hazel">hazel</option>
+                        <option value="gray">gray</option>
+                    </select>
+                </p>
+                <p>
+                    <input type="submit" value="send" />
+            </form>
         </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Hobby</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Caden</td>
+                    <td>Epoxy resin</td>
+                </tr>
+                <tr>
+                    <td>Bryce</td>
+                    <td>Baseball</td>
+                </tr>
+                <tr>
+            </tbody>
+        </table>
     </body>
 </html>
